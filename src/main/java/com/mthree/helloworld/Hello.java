39d63684dee1f8716ae6c3561cefaeae70eb7e82
ash-g773 <aishwaryageorge@ymail.com>
2022-10-11 15:09:38 +0100

    First commit

---
+++ src/main/java/com/mthree/helloworld/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.helloworld;
+
+/**
+ *
+ * @author ash_7
+ * email: aishwaryageorge@ymail.com
+ * date: 11/10/2022
+ * purpose: My first Hello World program in an IDE
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Ash!");
+    }
+}
